33500666704dca93265c9248a56c35bb6b7ea1f2
Alex Wan <a_wanderous@hotmail.com>
2019-10-24 01:04:14 +1100

    added questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,37 +6,37 @@ var friends = [{
     {
         name: "Anakin",
         photo: "https://vignette.wikia.nocookie.net/worldsgreatestheroes/images/5/59/Anakin_skywalker.jpg/revision/latest/scale-to-width-down/274?cb=20140601130537",
-        scores: []
+        scores: [1, 1, 3, 3, 3, 5, 4]
     },
     {
         name: "Donald",
         photo: "https://cdn1.spiegel.de/images/image-1402354-860_poster_16x9-bygn-1402354.jpg",
-        scores: []
+        scores: [5, 5, 5, 5, 1, 1, 5]
     },
     {
         name: "Peter",
         photo: "https://st1.bollywoodlife.com/wp-content/uploads/2019/06/Tom-Holland-Uncharted.jpg",
-        scores: []
+        scores: [4, 5, 1, 2, 5, 2, 3]
     },
     {
         name: "Amy",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuPOkV2g1vGmrjdY988VELYa0rTVqWqSkB1FwzmRlyNAwe0SSVlg",
-        scores: []
+        scores: [2, 2, 5, 3, 2, 4, 5]
     },
     {
         name: "Hinata",
         photo: "https://i.pinimg.com/originals/6c/4d/d2/6c4dd2ea531cb328d8ff4c92596b045a.jpg",
-        scores: []
+        scores: [5, 4, 2, 3, 5, 1, 3]
     },
     {
         name: "Jake",
         photo: "https://www.fjackets.com/product_images/c/571/Brooklyn_99_Andy_Samberg_Jacket__68905_zoom.jpg",
-        scores: []
+        scores: [3, 4, 3, 1, 1, 2, 2]
     },
     {
         name: "John",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt_j5fNEvFoRZMIC-FyCv5egy-CK3nsM7VPwpvDmoRBSazJ3DX",
-        scores: []
+        scores: [5, 2, 5, 1, 1, 3, 3]
     }
 ];
 
