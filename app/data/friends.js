c1b56ac261a430bfc16cd31b1c49db5b41b20230
Alex Wan <a_wanderous@hotmail.com>
2019-10-23 21:29:52 +1100

    add default names to list of friends

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+var friends = [{
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Anakin",
+        photo: "https://vignette.wikia.nocookie.net/worldsgreatestheroes/images/5/59/Anakin_skywalker.jpg/revision/latest/scale-to-width-down/274?cb=20140601130537",
+        scores: []
+    },
+    {
+        name: "Donald",
+        photo: "https://cdn1.spiegel.de/images/image-1402354-860_poster_16x9-bygn-1402354.jpg",
+        scores: []
+    },
+    {
+        name: "Peter",
+        photo: "https://st1.bollywoodlife.com/wp-content/uploads/2019/06/Tom-Holland-Uncharted.jpg",
+        scores: []
+    },
+    {
+        name: "Amy",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuPOkV2g1vGmrjdY988VELYa0rTVqWqSkB1FwzmRlyNAwe0SSVlg",
+        scores: []
+    },
+    {
+        name: "Hinata",
+        photo: "https://i.pinimg.com/originals/6c/4d/d2/6c4dd2ea531cb328d8ff4c92596b045a.jpg",
+        scores: []
+    },
+    {
+        name: "Jake",
+        photo: "https://www.fjackets.com/product_images/c/571/Brooklyn_99_Andy_Samberg_Jacket__68905_zoom.jpg",
+        scores: []
+    },
+    {
+        name: "John",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt_j5fNEvFoRZMIC-FyCv5egy-CK3nsM7VPwpvDmoRBSazJ3DX",
+        scores: []
+    }
+];
+
+module.exports = friends;
